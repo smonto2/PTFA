0f28f48f749160519072d2adc7994e04b7038eca
Santiago Montoya-Blandón <smontoya@outlook.com>
2024-11-14 01:55:47 +0000

    Final classes and examples under new name

--- src/ptfm/ptfm.py
+++ src/ptfm/ptfm.py
@@ -3,7 +3,7 @@ from scipy import linalg, sparse
 from sklearn.metrics import r2_score
 import warnings
 
-class ProbabilisticPLS:
+class ProbabilisticTFM:
     def __init__(self, n_components):
         # Fill in components of the class
         self.n_components = n_components
@@ -143,7 +143,7 @@ class ProbabilisticPLS:
             predicted_variance = self.sigma2_y * np.eye(q) + self.Q @ np.linalg.solve(Omega_X_inverse, self.Q.T)
             return Y_hat, predicted_variance
 
-class ProbabilisticPLS_Missing:
+class ProbabilisticTFM_Missing:
     def __init__(self, n_components):
         # Fill in components of the class
         self.n_components = n_components
@@ -171,19 +171,21 @@ class ProbabilisticPLS_Missing:
         _, q = Y.shape
         d = p + q
 
-        # Obtain indices of missing observations
-        X_missing_index = np.isnan(X)
-        Y_missing_index = np.isnan(Y)
+        # Obtain indices of missing observations to create masked objects
+        if not np.ma.isMaskedArray(X):
+            X_missing_index = np.isnan(X)
+            X = np.ma.MaskedArray(data=X, mask=X_missing_index, fill_value=0.0)
+        if not np.ma.isMaskedArray(Y):
+            Y_missing_index = np.isnan(Y)
+            Y = np.ma.MaskedArray(data=Y, mask=Y_missing_index, fill_value=0.0)
 
         # Center and scale predictors and targets separately if necessary
         if standardize:
-            X = (X - X.mean(axis = 0, where = np.logical_not(X_missing_index))) / X.std(axis = 0, where = np.logical_not(X_missing_index))
-            Y = (Y - Y.mean(axis = 0, where = np.logical_not(Y_missing_index))) / Y.std(axis = 0, where = np.logical_not(Y_missing_index))
+            X = (X - X.mean(axis = 0)) / X.std(axis = 0)
+            Y = (Y - Y.mean(axis = 0)) / Y.std(axis = 0)
 
         # Initial imputation step before stacking
-        X[X_missing_index] = 0
-        Y[Y_missing_index] = 0
-        Z = np.hstack([X, Y])
+        Z = np.hstack([X.filled(0.0), Y.filled(0.0)])
         Z_vars = np.var(Z, axis = 0)
 
         # Initial values for the parameters
@@ -204,17 +206,15 @@ class ProbabilisticPLS_Missing:
             
             # Update missing data using current EM fitted values
             Z_hat = M @ L0.T
-            X[X_missing_index] = Z_hat[:, :p][X_missing_index]
-            Y[Y_missing_index] = Z_hat[:, p:][Y_missing_index]
-            Z = np.hstack([X, Y])
+            Z = np.hstack([X.filled(Z_hat[:, :p]), Y.filled(Z_hat[:, p:])])
 
             # Maximization step: Update factor loadings and variances
             V = T * Omega + M.T @ M
             L1 = np.linalg.solve(V, M.T @ Z).T
             P = L1[:p]
             Q = L1[p:]
-            sigma2_x1 = (1/(T * p)) * (np.sum(X**2) - np.trace(P.T @ P @ V))
-            sigma2_y1 = (1/(T * q)) * (np.sum(Y**2) - np.trace(Q.T @ Q @ V))
+            sigma2_x1 = (1/(T * p)) * (np.sum(Z[:, :p]**2) - np.trace(P.T @ P @ V))
+            sigma2_y1 = (1/(T * q)) * (np.sum(Z[:, p:]**2) - np.trace(Q.T @ Q @ V))
             
             # Compute distance between iterates
             P_distance = np.linalg.norm(P - L0[:p], "fro")
@@ -226,9 +226,7 @@ class ProbabilisticPLS_Missing:
             # Prediction and tracking of R-squared across iterations
             if track_r2:
                 Y_hat = M @ Q.T
-                r2_values = r2_score(Y[np.logical_not(Y_missing_index)],
-                                     Y_hat[np.logical_not(Y_missing_index)],
-                                     multioutput = "raw_values")
+                r2_values = r2_score(Y, Y_hat, multioutput = "raw_values")
                 r2_list.append(r2_values)
 
             # Check convergence condition
@@ -247,29 +245,28 @@ class ProbabilisticPLS_Missing:
         self.sigma2_x = sigma2_x1
         self.sigma2_y = sigma2_y1
         self.r2_array = np.asarray(r2_list) if track_r2 else None
-        self.Z_hat_ = Z_hat
+        self.Z_imputed = Z
 
     def fitted(self, X, Y, standardize = False, prediction_variance = False):
-        # Obtain indices of missing observations
-        X_missing_index = np.isnan(X)
-        Y_missing_index = np.isnan(Y)
-        
-        # Center and scale predictors and targets separately if necessary
-        p = self.P.shape[0]
-        if standardize:
-            X = (X - X.mean(axis = 0, where = np.logical_not(X_missing_index))) / X.std(axis = 0, where = np.logical_not(X_missing_index))
-            Y = (Y - Y.mean(axis = 0, where = np.logical_not(Y_missing_index))) / Y.std(axis = 0, where = np.logical_not(Y_missing_index))
+        # Obtain indices of missing observations to create masked objects
+        if not np.ma.isMaskedArray(X):
+            X_missing_index = np.isnan(X)
+            X = np.ma.MaskedArray(data=X, mask=X_missing_index, fill_value=0.0)
+        if not np.ma.isMaskedArray(Y):
+            Y_missing_index = np.isnan(Y)
+            Y = np.ma.MaskedArray(data=Y, mask=Y_missing_index, fill_value=0.0)
 
-        # Fill missing values with prediction from model
-        X[X_missing_index] = self.Z_hat_[:, :p][X_missing_index]
-        Y[Y_missing_index] = self.Z_hat_[:, p:][Y_missing_index]
-        Z = np.hstack([X, Y])
+        # Center and scale predictors and targets separately if necessary
+        if standardize:
+            X = (X - X.mean(axis = 0)) / X.std(axis = 0)
+            Y = (Y - Y.mean(axis = 0)) / Y.std(axis = 0)
         
         # Obtain predicted factors: F_predicted = M (Posterior mean of factors)
         L = np.vstack([self.P, self.Q])
         L_scaled = np.vstack([self.P / self.sigma2_x, self.Q / self.sigma2_y])
         Omega_inverse = self.V_prior_inv + L.T @ L_scaled
-        F_predicted = np.linalg.solve(Omega_inverse, L_scaled.T @ Z.T).T
+        F_predicted = np.linalg.solve(Omega_inverse, L_scaled.T @ self.Z_imputed.T).T
+        # Z_imputed is saved from fit
 
         # Predict using estimated factors and update class
         self.factors = F_predicted
@@ -284,16 +281,21 @@ class ProbabilisticPLS_Missing:
             return Y_hat, fitted_variance
     
     def predict(self, X, standardize = False, prediction_variance = False, method = "mean"):
-        # Obtain indices of missing observations
-        X_missing_index = np.isnan(X)
-        
-        # Center and scale predictors
-        p = self.P.shape[0]
+        # Obtain indices of missing observations to create masked objects
+        if not np.ma.isMaskedArray(X):
+            X_missing_index = np.isnan(X)
+            X = np.ma.MaskedArray(data=X, mask=X_missing_index, fill_value=0.0)
+        if not np.ma.isMaskedArray(Y):
+            Y_missing_index = np.isnan(Y)
+            Y = np.ma.MaskedArray(data=Y, mask=Y_missing_index, fill_value=0.0)
+
+        # Center and scale predictors and targets separately if necessary
         if standardize:
-            X = (X - X.mean(axis = 0, where = np.logical_not(X_missing_index))) / X.std(axis = 0, where = np.logical_not(X_missing_index))
+            X = (X - X.mean(axis = 0)) / X.std(axis = 0)
         
         # Fill missing values with prediction from model
-        X[X_missing_index] = self.Z_hat_[:, :p][X_missing_index]
+        p = X.shape[1]
+        X = X.filled(self.Z_imputed[:, :p])
 
         # Explore differences in prediction methods
         P_scaled = self.P / self.sigma2_x
@@ -317,7 +319,7 @@ class ProbabilisticPLS_Missing:
             predict_variance = self.sigma2_y * np.eye(q) + self.Q @ np.linalg.solve(Omega_X_inverse, self.Q.T)
             return Y_hat, predict_variance
 
-class ProbabilisticPLS_MixedFrequency:
+class ProbabilisticTFM_MixedFrequency:
     def __init__(self, n_components):
         # Fill in components of the class
         self.n_components = n_components
@@ -500,7 +502,7 @@ class ProbabilisticPLS_MixedFrequency:
         Y_hat = (1/periods) * F_sum @ self.Q.T
         return Y_hat
 
-class ProbabilisticPLS_StochasticVolatility:
+class ProbabilisticTFM_StochasticVolatility:
     def __init__(self, n_components):
         # Fill in components of the class
         self.n_components = n_components
@@ -511,25 +513,6 @@ class ProbabilisticPLS_StochasticVolatility:
         self.sigma2_x = None
         self.sigma2_y = None
         self.factors = None
-
-    # def update_ewma_volatility(self, residuals, lambda_factor):
-    #     """
-    #     Compute EWMA volatility for the residuals over time.
-    #     :param residuals: Residuals of the model (T x p) where T is time and p is the number of features.
-    #     :param lambda_factor: The EWMA decay factor (0 < lambda_factor < 1).
-    #     :return: A (T,) array with the time-varying volatility.
-    #     """
-    #     T = residuals.shape[0]  # Get number of time points
-    #     sigma2 = np.zeros(T)    # Initialize a vector to store the variances over time
-
-    #     # Initialize the first variance estimate (across all features, isotropic assumption)
-    #     sigma2[0] = np.mean(residuals[0, :]**2)  # Mean variance at t = 0
-        
-    #     # Apply EWMA for each time step
-    #     for t in range(1, T):
-    #         sigma2[t] = lambda_factor * sigma2[t-1] + (1 - lambda_factor) * np.mean(residuals[t, :]**2)
-
-    #     return sigma2  # Return the variance (no need for square root since it's variance)
 
     def fit(self, X, Y, standardize = False, track_r2 = False, tolerance = 1e-6, max_iter = 1000, V_prior = None,
                  ewma_lambda_x = 0.94, ewma_lambda_y = None):
@@ -561,8 +544,8 @@ class ProbabilisticPLS_StochasticVolatility:
         L0 = np.random.default_rng().normal(size = [d, k])
         sigma2_x_initial = np.var(X, axis = 0).mean()    # Mean variance across features
         sigma2_y_initial = np.var(Y, axis = 0).mean()    # Mean variance across targets
-        sigma2_x0 = np.full(T, sigma2_x_initial)
-        sigma2_y0 = np.full(T, sigma2_y_initial)
+        sigma2_x = np.full(T, sigma2_x_initial)
+        sigma2_y = np.full(T, sigma2_y_initial)
 
         # Track R-squared of fit if necessary
         if track_r2:
@@ -574,7 +557,7 @@ class ProbabilisticPLS_StochasticVolatility:
         for _ in range(self.max_iter):
             # Expectation step: Update posterior paramater for factors
             for t in range(T):
-                L_scaled_t = np.vstack([L0[:p] / sigma2_x0[t], L0[p:] / sigma2_y0[t]])
+                L_scaled_t = np.vstack([L0[:p] / sigma2_x[t], L0[p:] / sigma2_y[t]])
                 Omega[t] = np.linalg.inv(self.V_prior_inv + L0.T @ L_scaled_t)
                 M[t] = Z[t] @ L_scaled_t @ Omega[t]
             V = np.sum(Omega, axis = 0) + M.T @ M
@@ -587,21 +570,18 @@ class ProbabilisticPLS_StochasticVolatility:
             # Update volatilities using EWMA
             Z_hat = M @ L1.T
             residuals_Z = Z - Z_hat
-            sigma2_x1 = np.copy(sigma2_x0); sigma2_y1 = np.copy(sigma2_y0)
-            sigma2_x1[0] = (1/p) * (np.sum(residuals_Z[0, :p]**2) + np.trace(P.T @ P @ Omega[0]))
-            sigma2_y1[0] = (1/q) * (np.sum(residuals_Z[0, p:]**2) + np.trace(Q.T @ Q @ Omega[0]))
+            sigma2_x[0] = (1/p) * (np.sum(residuals_Z[0, :p]**2) + np.trace(P.T @ P @ Omega[0]))
+            sigma2_y[0] = (1/q) * (np.sum(residuals_Z[0, p:]**2) + np.trace(Q.T @ Q @ Omega[0]))
             for t in range(1, T):
                 hat_sigma2_x_t = (1/p) * (np.sum(residuals_Z[t, :p]**2) + np.trace(P.T @ P @ Omega[t]))
                 hat_sigma2_y_t = (1/q) * (np.sum(residuals_Z[t, p:]**2) + np.trace(Q.T @ Q @ Omega[t]))
-                sigma2_x1[t] = self.ewma_lambda_x * sigma2_x1[t-1] + (1 - self.ewma_lambda_x) * hat_sigma2_x_t
-                sigma2_y1[t] = self.ewma_lambda_y * sigma2_y1[t-1] + (1 - self.ewma_lambda_y) * hat_sigma2_y_t
+                sigma2_x[t] = self.ewma_lambda_x * sigma2_x[t-1] + (1 - self.ewma_lambda_x) * hat_sigma2_x_t
+                sigma2_y[t] = self.ewma_lambda_y * sigma2_y[t-1] + (1 - self.ewma_lambda_y) * hat_sigma2_y_t
             
             # Compute distance between iterates
             P_distance = np.linalg.norm(P - L0[:p], "fro")
             Q_distance = np.linalg.norm(Q - L0[p:], "fro")
-            sigma_x_distance = np.linalg.norm(sigma2_x1 - sigma2_x0)
-            sigma_y_distance = np.linalg.norm(sigma2_y1 - sigma2_y0)
-            theta_distance = sum([P_distance, Q_distance, sigma_x_distance, sigma_y_distance])
+            theta_distance = sum([P_distance, Q_distance])
             
             # Prediction and tracking of R-squared across iterations
             if track_r2:
@@ -615,14 +595,12 @@ class ProbabilisticPLS_StochasticVolatility:
             else:
                 # Prepare values for next iteration if convergence not reached
                 L0 = L1
-                sigma2_x0 = sigma2_x1
-                sigma2_y0 = sigma2_y1
                 
         # Update final values of the class with results from EM algorithm
         self.P = P
         self.Q = Q
-        self.sigma2_x = sigma2_x1
-        self.sigma2_y = sigma2_y1
+        self.sigma2_x = sigma2_x
+        self.sigma2_y = sigma2_y
         self.r2_array = np.asarray(r2_list) if track_r2 else None
         
     def fitted(self, X, Y, standardize = False):
@@ -679,7 +657,7 @@ class ProbabilisticPLS_StochasticVolatility:
         #     predicted_variance = self.sigma2_y * np.eye(q) + self.Q @ np.linalg.solve(Omega_X_inverse, self.Q.T)
         #     return Y_hat, predicted_variance
 
-class ProbabilisticPLS_DynamicFactors:
+class ProbabilisticTFM_DynamicFactors:
     def __init__(self, n_components):
         # Fill in components of the class
         self.n_components = n_components
@@ -693,12 +671,12 @@ class ProbabilisticPLS_DynamicFactors:
         self.f0 = None
         self.factors = None
 
-    def bands_cholesky(self, cholesky_banded, desired_bands):
+    def bands_cholesky(self, cholesky_banded, desired_bands=0):
         """
         Input:
-            cholesky_banded - (2 * k x T * k) banded matrix with the Cholesky decomposition of Omega_inverse
+            cholesky_banded - (l * k x T * k) banded matrix with the Cholesky decomposition of Omega_inverse
                 Cholesky decomposition of Omega^(-1) (T * k x T * k), where k = n_components
-                Omega_inverse is assumed banded with lower bandwidth = 2 * k - 1 or total_bandwidth = 4 * k - 1
+                Omega_inverse is assumed banded with lower bandwidth = l * k - 1 or total_bandwidth = l * k - 1
             desired_bands   - Number of total bands to calculate (if set to 0, only calculates main diagonal)
         Output:
             Omega - (desired_bands * k, T * k) banded matrix with inverse elements
@@ -706,8 +684,7 @@ class ProbabilisticPLS_DynamicFactors:
                 Element (i, j) of Omega is located in [maximum_bands + i - j, t*k + r] for l, r in {0, ..., k-1} 
         """
         # Pre-allocate final objective using both the upper and lower bands for now
-        Tk = cholesky_banded.shape[1]
-        lower_bandwidth = cholesky_banded.shape[0] - 1
+        half_bandwidth, Tk = cholesky_banded.shape
         total_rows = 2 * desired_bands + 1
         Omega = np.zeros([total_rows, Tk])
         
@@ -717,12 +694,12 @@ class ProbabilisticPLS_DynamicFactors:
         cholesky_diagonal = 1 / cholesky_diagonal**2
         
         # Main algorithm loop
-        bandwidth_range = range(1, lower_bandwidth+1)
+        bandwidth_range = range(1, half_bandwidth)
         for j in reversed(range(Tk)):
             for i in reversed(range(max(j - desired_bands - 1, 0), j)):
                 save_row_index = desired_bands + i - j
                 next_row_index = range(min(save_row_index + 1, total_rows - 1),
-                                       min(save_row_index + lower_bandwidth + 1, total_rows - 1))
+                                       min(save_row_index + half_bandwidth, total_rows - 1))
                 Omega[save_row_index, j] = -np.dot(cholesky_banded[bandwidth_range, i], Omega[next_row_index, j])
                 Omega[min(desired_bands + j - i, total_rows - 1), i] = Omega[save_row_index, j]
                 if i == j:
